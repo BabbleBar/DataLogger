0347a8bdec0dd8932737a193bfa047a05dab6671
Florian Schrag <florian@schrag.org>
2015-11-07 14:31:02 +0100

    cleaner log output

--- logger.py
+++ logger.py
@@ -60,7 +60,8 @@ def receive_new_message(ch, method, properties, body):
                                    data['payload_hex']
                                    ))
     result = db.full_data.insert_one(data)
-    print("Inserted into MongoDB: " % result.inserted_id)
+    print("Inserted into MongoDB: ")
+    pprint.pprint(result.inserted_id)
 
 
 def start_listener():
