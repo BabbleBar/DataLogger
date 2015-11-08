fccd180037e08b6ae963fba503672b489438009e
Florian Schrag <florian@schrag.org>
2015-11-08 01:49:23 +0100

    add origin *

--- logger.py
+++ logger.py
@@ -3,14 +3,61 @@ import json
 import os
 import pprint
 import threading
-from dateutil.parser import parse
+from datetime import timedelta
+from functools import update_wrapper
+
 import pika
 import pymongo
-from flask import Flask
-from pymongo import MongoClient
 from bson.json_util import dumps
+from dateutil.parser import parse
+from flask import Flask
+from flask import make_response, request, current_app
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+
+    return decorator
 
 
 @app.route("/ping")
@@ -18,6 +65,7 @@ def hello():
     return "pong"
 
 
+@crossdomain(origin='*')
 @app.route("/type/<data_type>/avg/minutes/<int:minutes>")
 def avg(data_type, minutes):
     return dumps(db[data_type].aggregate(
@@ -35,6 +83,7 @@ def avg(data_type, minutes):
     ))
 
 
+@crossdomain(origin='*')
 @app.route("/eui/<eui>/type/<data_type>/avg/minutes/<int:minutes>")
 def avg_eui(eui, data_type, minutes):
     return dumps(db[data_type].aggregate(
@@ -53,6 +102,7 @@ def avg_eui(eui, data_type, minutes):
     ))
 
 
+@crossdomain(origin='*')
 @app.route("/eui/<eui>/type/<data_type>/last/<int:limit>")
 def full_data(eui, data_type, limit):
     cursor = db[data_type].find({'eui': eui}).sort([("timestamp", pymongo.DESCENDING)]).limit(limit)
